b111b4a3edeaf0ee438549dcbddf0c99caff168b
bigvic135 <victoree@umich.edu>
2018-11-07 19:30:42 -0500

    Add files via upload

---
+++ len_ext_attack.py
@@ -0,0 +1,10 @@
+import httplib, urlparse, sys
+url = sys.argv[1]
+
+# Your code to modify url goes here
+parsedUrl = urlparse.urlparse(url)
+conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
+conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
+print conn.getresponse().read()
+
+i got part 2.2 and 2.3
